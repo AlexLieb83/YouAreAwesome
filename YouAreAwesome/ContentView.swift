9f0746b6320b5ae3980c6c82a03f75a21dc000ab
AlexLieb83 <alexliebhaber1992@gmail.com>
2025-11-02 13:58:00 -0600

    trying

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You are awesome!")
+            Text("I am a developer!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.cyan)
